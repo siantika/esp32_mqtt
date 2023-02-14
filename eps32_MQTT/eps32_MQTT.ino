15a8e4384c5bb3df8a8f03bff2e639c02da11bb0
I Putu Pawesi Siantika <csiantika@gmail.com>
2023-02-15 00:48:03 +0800

    membuat fungsi - fungsi pembacaan sensor

--- eps32_MQTT/eps32_MQTT.ino
+++ eps32_MQTT/eps32_MQTT.ino
@@ -1,7 +1,12 @@
 
+/* ********** pin ********** */
+#define PIN_FLAME_SENSOR 2  // analog pin 
+#define PIN_POWER_IN_VOLTAGE 15
+#define V_REF 3.3
+#define MAX_ADC_RES 4096
+
 
 /* interface C++ ke C */
-
 #ifdef __cplusplus
 
 extern "C" {
@@ -15,32 +20,71 @@ uint8_t temprature_sens_read();
 }
 
 #endif
-/* ********* berakhir di sini ********** */
+/* ********** berakhir di sini ********** */
+
 
 uint8_t temprature_sens_read(); // jangan diubah!
 
 
-// initialisasi nama fungsi
-
+/* ********** inisialisasi nama fungsi ********** */
 float getInternalTemperature(void);
 float getInternalVoltage(void);
-float getRawDataIr(void);
+uint16_t getRawDataFlameSensor(void);
 
 
-
-
-
+/* ********** Driver Code ********** */
 void setup() {
   Serial.begin(115200);
+
 
 }
 
 void loop() {
+  
 
-    
-//  float temp = (float)(temprature_sens_read() - 32) / 1.8;
+}
 
-  Serial.println(temprature_sens_read());
-  delay(500);
+/* ********** Driver Code  Berakhir di sini ********** */
+
+
+/* Fungsi - Fungsi */
+
+/*  Nama        : getInternalTemperature
+    Deskripsi   : Membaca nilai temperature internal dari ESP32. Harus mendeklarasikan interface C ke C++ guard di atas
+                  dan mendeklarasikan fungsi temperature_sens_read() dengan tipe data uint8_t
+    Param       : void
+    Return value: (float) nilai temperatur dalam celcius
+*/
+float getInternalTemperature()
+{
+  return (float) (temprature_sens_read() - 32) / 1.8;
+}
+
+
+/*  Nama        : getRawDataFlameSensor
+    Deskripsi   : Mendapatkan nilai mentah dari flame sensor. Jika sensor mendeteksi api, maka nilai yang didapat akan menurun dan sebaliknya.
+                  Rentang nilai (0-4095)
+    Param       : void
+    Return value: (uint16_t) nilai mentah sensor (ADC)
+*/
+uint16_t getRawDataFlameSensor()
+{
+  return analogRead(PIN_FLAME_SENSOR);
+}
+
+/*  Nama        : getInternalVoltage
+    Deskripsi   : mendapatkan nilai tegangan dalam satuan volt dari sumber daya ESP32
+    Param       : void
+    Return value: (float) nilai tegangan dalam volt
+*/
+float getInternalVoltage()
+{
+  int _rawDataAdc = 0;
+  float _powerInVoltage = 0;
+  
+  _rawDataAdc = analogRead(PIN_POWER_IN_VOLTAGE);
+  _powerInVoltage = _rawDataAdc * V_REF / MAX_ADC_RES;
+
+  return _powerInVoltage;
 
 }
